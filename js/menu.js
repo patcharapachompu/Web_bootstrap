2ded6f76c613b985d74e5748fa52883d9fee88bd
patcharapachompu <patcharapa5998@gmail.com>
2026-09-20 09:59:54 -0700

    update v1.3

--- js/menu.js
+++ js/menu.js
@@ -12,3 +12,17 @@ fetch("menu.html")
                 });
             }
         });
+fetch("footer.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+
+            // ✅ re-bind toggle หลังจากเมนูถูก inject
+            const sidebarToggle = document.body.querySelector('#sidebarToggle');
+            if (sidebarToggle) {
+                sidebarToggle.addEventListener('click', event => {
+                    event.preventDefault();
+                    document.body.classList.toggle('sb-sidenav-toggled');
+                });
+            }
+        });
